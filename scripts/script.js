71933b50607f911bc883578f47d81bfe4d203ed7
Kenji Hirabayashi <mkhira2@gmail.com>
2017-02-20 18:26:56 -0600

    basic layout done

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+var getLegislatorData = function(sunlightObj) {
+	var legislatorStr = ''
+	legislatorStr += '<div class="legislator">' + '<h1 class="name">' + sunlightObj.first_name + ' ' +sunlightObj.last_name + '</h1>'
+	legislatorStr += '<h2 class="partyAndState">' + sunlightObj.title + ' -- ' + sunlightObj.party + ' - ' + sunlightObj.state_name + '</h2>'
+	legislatorStr += 	'<ul class="contact">'
+	legislatorStr += 		'<li>' + 'office: ' + sunlightObj.office + '</li>' 
+	legislatorStr += 		'<li>' + 'website: ' + sunlightObj.website + '</li>' 
+	legislatorStr += 		'<li>' + 'facebook: ' +sunlightObj.facebook_id + '</li>'
+	legislatorStr += 		'<li>' + 'twitter: ' + sunlightObj.twitter_id + '</li>'
+	legislatorStr += 	'</ul>'
+	legislatorStr += '<h3 class="term_end">' + sunlightObj.term_end + '</h3>' + '</div>'
+
+	return legislatorStr
+}
+
+var handleResponse = function(apiResponse) {
+	var stringHTML = ''
+	var containerNode = document.querySelector('.container')
+
+	for (var i = 0; i < apiResponse.results.length; i++) {
+		stringHTML += getLegislatorData(apiResponse.results[i])
+	}
+	containerNode.innerHTML = stringHTML
+}
+
+
+var promise = $.getJSON('https://congress.api.sunlightfoundation.com/legislators')
+
+promise.then(handleResponse)
